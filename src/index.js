7cf8aaa4588c6c0b37eedfad95173f8340865e4c
PavelZakharov <pavelzakharov1987@gmail.com>
2020-03-22 16:01:19 +0300

    feat: add script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function toReadable (number) {
-  
+    var a = [
+        ["zero", "one", "two", "three", "four", "five", "six",
+            "seven", "eight", "nine", "ten", "eleven",
+            "twelve", "thirteen", "fourteen", "fifteen",
+            "sixteen", "seventeen", "eighteen", "nineteen"],
+    
+        [,,"twenty", "thirty", "forty", "fifty", "sixty",
+            "seventy", "eighty", "ninety"]
+    ];
+    number = parseInt(number, 10);
+    if (number < 20) {
+        return a[0][number];
+    }
+    if (number < 100) {
+        return (a[1][parseInt(number / 10)] + ' ' + ((number % 10 === 0) ? '' : toReadable(number % 10))).trim();
+    }
+    return (a[0][parseInt(number / 100)] + ' hundred' + ' ' + ((number % 100 === 0) ? '' : toReadable(('' + number).substr(1)))).trim();
 }
